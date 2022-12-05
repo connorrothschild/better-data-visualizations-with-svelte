bd2ff1574bd1853879e8ed5c798a5184666c70d9
Connor Rothschild <connorrothschild@gmail.com>
2022-12-04 20:59:29 -0600

    Adding deployed files

--- test.sh
+++ test.sh
@@ -20,6 +20,8 @@ mkdir -p docs
 
 # Loop through all directories in the current directory again, excluding the dist/ folder
 for d in $(find . -maxdepth 1 -type d ! -path "./docs"); do
+    # Remove .git from the directory name
+    dir=$(echo $d | sed 's/.\///')
   # Move the dist/ folder from the current directory into the deployed/ folder
   # But first rename the dist/ folder to the name of the current directory
     mv $d/dist docs/$(basename $d)
