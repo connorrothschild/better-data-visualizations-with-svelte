95bb804f18fe88cae7953e67ff3b6f459d6af9e7
Connor Rothschild <connorrothschild@gmail.com>
2022-12-04 22:05:18 -0600

    Cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# Abort on errors
 set -e
 
-# create a build folder and clear it if it exists
+# Create a build folder and clear it if it exists
 rm -rf docs
 mkdir -p docs
 
@@ -11,19 +11,14 @@ mkdir -p docs
 for dir in */; do
   if [ -f "$dir/package.json" ]; then
     cd $dir
+    # Install dependencies
     npm install
     npm run build
-    # Delete all contents other than the docs folder
-    # rm -rf `ls | grep -v docs`
-    # Move the contents of the docs folder to the root of this directory
-    # mv docs/* .
     # Replace any reference of /assets/ with ./assets/
     find . -type f -name '*.html' -exec sed -i '' 's/\/assets\//\.\/assets\//g' {} +
     # Add .nojekyll to bypass Jekyll processing
     touch .nojekyll
-    # Delete all files and folders that are not .nojekyll, index.html, or assets/
-    # rm -rf `ls -a | grep -v .nojekyll | grep -v index.html | grep -v assets`
-    # rename the docs folder to the name of the directory
+    # Rename the docs folder to the name of the directory
     mv docs $dir
     # Move the contents of the docs folder to the root docs folder
     mv $dir ../docs
@@ -31,17 +26,12 @@ for dir in */; do
   fi
 done
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# Add and deploy
 git checkout -B master
 git add -A
 git commit -m 'Deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:connorrothschild/better-data-visualizations-with-svelte.git
 
 cd -
+
