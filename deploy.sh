70db43f430cd837efc6f6b50e86dcdc7d40a082f
Connor Rothschild <connorrothschild@gmail.com>
2022-12-04 22:14:29 -0600

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -7,24 +7,28 @@ set -e
 rm -rf docs
 mkdir -p docs
 
-# cd into each directory and run npm run build, if it has a package.json
-for dir in */; do
-  if [ -f "$dir/package.json" ]; then
-    cd $dir
-    # Install dependencies
-    npm install
-    npm run build
-    # Replace any reference of /assets/ with ./assets/
-    find . -type f -name '*.html' -exec sed -i '' 's/\/assets\//\.\/assets\//g' {} +
-    # Add .nojekyll to bypass Jekyll processing
-    touch .nojekyll
-    # Rename the docs folder to the name of the directory
-    mv docs $dir
-    # Move the contents of the docs folder to the root docs folder
-    mv $dir ../docs
-    cd ..
-  fi
-done
+# Loop through directories until a package.json file is found
+# found=0
+# while [ $found -eq 0 ]; do
+  for dir in */; do
+    if [ -f "$dir/package.json" ]; then
+    #   found=1
+      cd $dir
+      # Install dependencies
+      npm install
+      npm run build
+      # Replace any reference of /assets/ with ./assets/
+      find . -type f -name '*.html' -exec sed -i '' 's/\/assets\//\.\/assets\//g' {} +
+      # Add .nojekyll to bypass Jekyll processing
+      touch .nojekyll
+      # Rename the docs folder to the name of the directory
+      mv docs $dir
+      # Move the contents of the docs folder to the root docs folder
+      mv $dir ../docs
+      cd ..
+    fi
+  done
+# done
 
 # Add and deploy
 git checkout -B master
@@ -34,4 +38,3 @@ git commit -m 'Deploy'
 git push -f git@github.com:connorrothschild/better-data-visualizations-with-svelte.git
 
 cd -
-
