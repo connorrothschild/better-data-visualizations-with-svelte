a6c2e4a4a66a0f29245994179b761762d1ae1ba8
Connor Rothschild <connorrothschild@gmail.com>
2023-01-04 20:39:58 -0600

    No emojis

--- 01/02/src/components/AxisY.svelte
+++ 01/02/src/components/AxisY.svelte
@@ -8,7 +8,7 @@
 <g class='axis y'>
 {#each yTicks as tick, index}
 <g class='tick' transform="translate(0, {yScale(tick)})">
-  <line x1={0} x2={width} y1={0} y2={0} stroke={index === 0 ? '#8f8f8f' : '#e5e7eb'} /> // ✅
+  <line x1={0} x2={width} y1={0} y2={0} stroke={index === 0 ? '#8f8f8f' : '#e5e7eb'} /> 
   <text y={-3}>{index === yTicks.length - 1 ? `${tick} hours studied` : tick}</text>
 </g>
 {/each}
